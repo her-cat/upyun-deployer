0ddb54a52302163f1e339cee7dfbbbbef9183e9b
her-cat <hxhsoft@foxmail.com>
2023-07-28 14:38:42 +0800

    refactor: delete output arg

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/sh -l
 
-echo "bucket: $bucket operator: $operator password: $password dir: $dir"
+echo "bucket: $INPUT_BUCKET operator: $INPUT_OPERATOR password: $INPUT_PASSWORD dir: $INPUT_DIR"
 
 cd /opt/www/
 
@@ -10,8 +10,6 @@ go build .
 
 echo 'Deploying to UpYun...'
 
-output=`./upyun-deployer -bucket "$bucket" -operator "$operator" -password "$password" -dir "$dir"`
+./upyun-deployer -bucket "$INPUT_BUCKET" -operator "$INPUT_OPERATOR" -password "$INPUT_PASSWORD" -dir "$INPUT_DIR"
 
 echo 'Complete'
-
-echo "output=$output=" >> $GITHUB_OUTPUT
